d7c056a71adc0e325c51ac222a5bfb067cdd84b0
Evrard237-sudo <114961933+Evrard237-sudo@users.noreply.github.com>
2023-03-18 09:57:39 +0100

    first commit

---
+++ app/src/main/java/com/archcomp/notesyttutorial/Models/Note.kt
@@ -0,0 +1,16 @@
+package com.archcomp.notesyttutorial.Models
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+// ceci est data classe "Note" (c'est la table note)
+@Entity(tableName = "notes_table") // La on mentionne que la data classe a pour nom de table "notes_table"
+data class Note ( // Ceci sont les attributs ou propriétés de la table ou classe "Note"
+    @PrimaryKey(autoGenerate = true) val id: Int?, // On spécifie ici que id est la clé primaire et peut s'auto-incrémenté
+    @ColumnInfo(name = "title")/* Ici  on specifie le nom de la colonne de la table auquel appartient l'attribut */ var title: String?,
+    @ColumnInfo(name = "note")/* Idem */ val note: String?,
+    @ColumnInfo(name = "date") /* Idem */ val date: String?
+
+
+) : java.io.Serializable
